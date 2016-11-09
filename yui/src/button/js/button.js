7817a5ed294bb2697ca4d996d3fb2a933abf4a9c
Thomas Mellemseter <Thomas Mellemseter>
2016-11-09 16:59:29 +0100

    Make a dialogue appear on button click

--- yui/src/button/js/button.js
+++ yui/src/button/js/button.js
@@ -24,65 +24,95 @@
  /**
   * @module moodle-atto_syntaxhighlighter-button
   */
+var COMPONENTNAME = 'atto_syntaxhighlighter';
+var STYLE = {
+    CODEAREA: 'atto_syntaxhighlighter_codearea'
+};
+var SELECTORS = {
+    CODEAREA: '.atto_syntaxhighlighter_codearea'
+};
+
+var TEMPLATE = '<form class="atto_form">' +
+                    '<label for="{{elementid}}_atto_syntaxhighlighter_codearea">Some label</label>' +
+                    '<textarea class="fullwidth code {{style.CODEAREA}}" type="text"' +
+                              'id="{{elementid}}_atto_syntaxhighlighter_codearea"/><br>' +
+                    '<div class="mdl-align">' +
+                        '<br/>' +
+                        '<button type="submit" class="submit">Add codesnippet</button>' +
+                    '</div>' +
+                '</form>';
 
 var logic = {
+    _currentSelection: null,
+    _content: null,
     initializer: function() {
         this.addButton({
             icon: 'e/source_code',
-            callback: this._toggleSyntaxHighliting
+            callback: this._displayDialogue
         });
     },
-    _toggleSyntaxHighliting: function(event) {
-        this.set('isHighlightingEnabled', !this.get('isHighlightingEnabled'));
-        this._showHTML();
-    },
-    _showHTML: function() {
 
-        var phpcode = '<?php'
-                    + ' $variable = 100;'
-                    + '?>';
+    _displayDialogue: function() {
+        this._currentSelection = this.get('host').getSelection();
+        if (this._currentSelection === false) {
+            return;
+        }
 
-        var html = '<link rel="stylesheet" href="{{style}}">'
-                 + '<script src="{{lib}}"></script>'
-                 + '<pre><code class="php">'
-                 + '{{code}}'
-                 + '</code></pre>';
-
-        var template = Y.Handlebars.compile(html);
-        var test = template({
-            style: '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/styles/default.min.css',
-            lib: '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/highlight.min.js',
-            code: phpcode
+        var dialogue = this.getDialogue({
+            headerContent: 'Some header content', //M.util.get_string('Add code snippet', COMPONENTNAME),
+            focusAfterHide: true,
+            focusOnShowSelector: SELECTORS.CODEAREA
         });
 
-        // NOTE(thomas): How to insert this node into the texarea?
-        var content = Y.Node.create(test);
+        dialogue.set('bodyContent', this._getDialogueContent());
+        //this._resolveAnchors();
+        dialogue.show();
+    },
 
-        // NOTE(thomas):
-        // Can listen on valuechanges on the textarea,
-        // Might come in handy?
+    _setCode: function(event) {
+        event.preventDefault();
+        this.getDialogue({
+            focusAfterHide: null
+        }).hide();
+
+        var input = this._content.one('.code');
+
+        var value = input.get('value');
+
+        this._wrapCode(value);
+    },
+    _wrapCode: function(code) {
+        this.editor.focus();
         var host = this.get('host');
-        host.editor.on('valuechange', function() {
-            console.log('change');
-        });
+        host.setSelection(this._currentSelection);
 
-        // NOTE(thomas) Make the syntax button toggable.
-        if (!this.get('isHighlightingEnabled')) {
-            this.unHighlightButtons('html');
-            host.enablePlugins();
+        var collapsed = (this._currentSelection[0].collapsed);
+        var selectednode;
+
+        if (collapsed) {
+            var codenode = Y.Node.create('<code>' + code + '</code>');
+            codenode.setAttribute('class', 'PHP');
+            var prenode = Y.Node.create('<pre>' + codenode + '</pre>');
+
+            selectednode = host.insertContentAtFocusPoint(prenode.get('outerHTML'));
+            host.setSelection(host.getSelectionFromNode(selectednode));
         } else {
-            this.highlightButtons('html');
-            host.disablePlugins();
-            host.enablePlugins(this.name);
+            document.execCommand();
+            selectednode = host.getSelectionParentNode();
         }
-    }
-};
-
-var data = {
-    ATTRS: {
-        isHighlightingEnabled: {
-            value: false
+        if (!selectednode) {
+            return;
         }
+        return selectednode;
+    },
+    _getDialogueContent: function() {
+        var template = Y.Handlebars.compile(TEMPLATE);
+        this._content = Y.Node.create(template({
+            component: COMPONENTNAME,
+            style: STYLE
+        }));
+        //console.log(this._content);
+        //this._content.one('.submit').on('click', this._setCode, this);
     }
 };
 
@@ -93,6 +123,5 @@ var data = {
   * @class button
   * @extends M.editor_atto.EditorPlugin
   */
-
 Y.namespace('M.atto_syntaxhighlighter').Button =
-    Y.Base.create('button', Y.M.editor_atto.EditorPlugin, [], logic, data);
+    Y.Base.create('button', Y.M.editor_atto.EditorPlugin, [], logic);
